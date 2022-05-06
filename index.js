c76ba8424102b103af1138d9394c47b9334fe87a
solo-mizan <mizanbd.eee@gmail.com>
2022-05-07 00:53:53 +0600

    mongodb added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -8,15 +9,26 @@ const app = express();
 app.use(cors());
 app.use(express());
 
+// mongodb config
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.udusb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 async function run() {
     try {
+        await client.connect();
+        const warehouseCollection = client.db('warehouse').collection('products');
 
     }
     catch { }
 }
 
-run().catch(console.dir);
 
+
+
+
+
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Server is running for warehouse managemet');
