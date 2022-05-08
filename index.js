97d79f67dca167e15b6f3728f84820a99713fb29
solo-mizan <mizanbd.eee@gmail.com>
2022-05-08 23:33:37 +0600

    readme file added.

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
 
 //  middleware
 app.use(cors());
@@ -13,12 +14,38 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.udusb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function checkJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Unauthorize access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' });
+        }
+        console.log('decoded', decoded);
+        req.decoded = decoded;
+        next();
+    })
+}
+
 console.log(uri);
 
 async function run() {
     try {
         await client.connect();
         const warehouseCollection = client.db('warehouse').collection('items');
+
+        // json web token for login
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const tempToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            });
+            res.send({ tempToken });
+        })
+
 
         // items api
         app.get('/items', async (req, res) => {
@@ -52,10 +79,33 @@ async function run() {
             console.log(result);
             res.send(result);
         });
+
+        // get my products api
+        app.get('/myitem', async (req, res) => {
+            const userid = req.query.uid;
+            const query = { userid };
+            const cursor = warehouseCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
+        // get my item verification api
+        app.get('/myitem', checkJWT, async (req, res) => {
+            const emailDecoder = req.decoded.email;
+            const email = req.query.email;
+            if (email === emailDecoder) {
+                const query = { email: email };
+                const cursor = warehouseCollection.find(query);
+                const myitem = await cursor.toArray();
+                res.send(myitem);
+            }
+            else {
+                res.status(403).send({ message: 'forbidden access' })
+            }
+        })
     }
     catch { }
 }
-
 
 run().catch(console.dir);
 
